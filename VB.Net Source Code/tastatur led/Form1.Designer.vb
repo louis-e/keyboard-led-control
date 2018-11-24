eff05fe2717c65cdc3585c27f94c2f9b8b654189
louis-e <44675238+louis-e@users.noreply.github.com>
2018-11-24 15:12:51 +0100

    Add files via upload

---
+++ VB.Net Source Code/tastatur led/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(114, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Caps Lock"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Nummerblock"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(195, 23)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Rollen"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 500
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 500
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(95, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Led's einzeln ansteuern"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(129, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Effekte"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(114, 61)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Stop"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(16, 61)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(92, 23)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "Welle"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(195, 61)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "Impuls"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 500
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(294, 101)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Tastatur LEDs"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Timer4 As Timer
+End Class
---
+++ VB.Net Source Code/tastatur led/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+    '<deklarationen>
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Long, ByVal dwExtraInfo As Long)
+    Private Const KEYEVENTF_KEYUP = &H2
+    Const VK_NUMLOCK = &H90
+    Private Const VK_SCROLL As Short = &H91
+    Private Const KEYEVENTF_EXTENDEDKEY As Short = &H1
+    Private Const KEYEVENTF_KEYUP3 As Short = &H2
+    Const KEYEVENTF_EXTENDEDKEY3 = &H1
+    Declare Function GetKeyState Lib "user32" Alias "GetKeyState" (ByVal ByValnVirtKey As Integer) As Short
+    <DllImport("user32.dll")>
+    Private Shared Sub keybd_event(ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As UInteger, ByVal dwExtraInfo As Integer)
+    End Sub
+    Const VK_CAPITAL = &H14
+    Const KEYEVENTF_KEYUP2 = &H2
+    '</deklarationen>
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        keybd_event(VK_CAPITAL, 0, 0, 0)
+        keybd_event(VK_CAPITAL, 0, KEYEVENTF_KEYUP2, 0)
+
+        'capital = capslock (auf der Tastatur)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        keybd_event(VK_NUMLOCK, 0, 0, 0)
+        keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYUP2, 0)
+
+        'numlock = Num (auf der Tastatur)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or 0, 0)
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or KEYEVENTF_KEYUP3, 0)
+
+        'scroll = Rollen (auf der Tastatur)
+    End Sub
+
+
+
+    '<Effekt Welle>
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        keybd_event(VK_NUMLOCK, 0, 0, 0)
+        keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYUP2, 0)
+        Timer1.Enabled = False
+        Timer2.Start()
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        keybd_event(VK_CAPITAL, 0, 0, 0)
+        keybd_event(VK_CAPITAL, 0, KEYEVENTF_KEYUP2, 0)
+        Timer3.Start()
+        Timer2.Stop()
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or 0, 0)
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or KEYEVENTF_KEYUP3, 0)
+        Timer1.Start()
+        Timer3.Stop()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Timer1.Start()
+    End Sub
+    '</Effekt Welle>
+
+
+    '<alle effekte aus>
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'timer der effekte stoppen
+        Timer1.Stop()
+        Timer2.Stop()
+        Timer3.Stop()
+        Timer4.Stop()
+
+        'if abfrage: ist die jeweilige taste "gedrückt worden"?
+        If My.Computer.Keyboard.CapsLock Then
+            keybd_event(VK_CAPITAL, 0, 0, 0)
+            keybd_event(VK_CAPITAL, 0, KEYEVENTF_KEYUP2, 0)
+        End If
+        If My.Computer.Keyboard.NumLock Then
+            keybd_event(VK_NUMLOCK, 0, 0, 0)
+            keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYUP2, 0)
+        End If
+        If My.Computer.Keyboard.ScrollLock Then
+            keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or 0, 0)
+            keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or KEYEVENTF_KEYUP3, 0)
+        End If
+    End Sub
+    '</alle effekte aus>
+
+
+    '<effekt interval>
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or 0, 0)
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY3 Or KEYEVENTF_KEYUP3, 0)
+
+        keybd_event(VK_CAPITAL, 0, 0, 0)
+        keybd_event(VK_CAPITAL, 0, KEYEVENTF_KEYUP2, 0)
+
+        keybd_event(VK_NUMLOCK, 0, 0, 0)
+        keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYUP2, 0)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Timer4.Start()
+    End Sub
+    '</effekt interval>
+End Class
